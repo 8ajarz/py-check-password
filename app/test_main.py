8c3afa7d98228dcac13e0f1228cdce5dda18f4df
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-30 00:04:48 +0700

    cleaned up flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,25 @@
 from app.main import check_password
 
-def test_should_check_min_length():
+
+def test_should_check_min_length() -> None:
     assert check_password("S@1a") is False  # Too short
 
-def test_should_check_max_length():
-    assert check_password("A@1" + "x" * 20) is False  # Too long if max length is e.g., 16
 
-def test_should_check_upper_letter():
+def test_should_check_max_length() -> None:
+    assert check_password("A@1" + "x" * 20) is False  # Too long
+
+
+def test_should_check_upper_letter() -> None:
     assert check_password("lower@1word") is False  # No uppercase
 
-def test_should_check_digit():
+
+def test_should_check_digit() -> None:
     assert check_password("NoDigits@") is False  # No digits
 
-def test_should_check_special_symbols():
+
+def test_should_check_special_symbols() -> None:
     assert check_password("NoSpecial1") is False  # No special characters
 
-def test_should_accept_valid_password():
+
+def test_should_accept_valid_password() -> None:
     assert check_password("Val1d@Pass") is True  # Meets all criteria
